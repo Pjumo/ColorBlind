aa4f9c2fcc7059b0c2b8ccc7a15adc9760e65145
Pjumo <parkjunmozz55@gmail.com>
2022-07-29 00:06:28 +0900

    cameraKitView 적용

--- app/src/main/java/com/onblur/colorblind/MainActivity.kt
+++ app/src/main/java/com/onblur/colorblind/MainActivity.kt
@@ -2,10 +2,46 @@ package com.onblur.colorblind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.camerakit.CameraKitView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraKitView: CameraKitView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cameraKitView = findViewById(R.id.main_camera)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cameraKitView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cameraKitView.onResume()
+
+        cameraKitView.captureImage { view, capturedImage ->
+
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        cameraKitView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cameraKitView.onStop()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
